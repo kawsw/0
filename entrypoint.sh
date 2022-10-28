1ef9a354354f6e71f0934616a724c83bae74a21e
kawsw <116861413+kawsw@users.noreply.github.com>
2022-10-27 22:29:48 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ CADDYIndexPage="https://raw.githubusercontent.com/caddyserver/dist/master/welcom
 # CONFIGCADDY="https://raw.githubusercontent.com/kiujhyt/xray3/master/etc/Caddyfile"
 # CONFIGXRAY="https://raw.githubusercontent.com/kiujhyt/xray3/master/etc/xray.json"
 ParameterSSENCYPT="chacha20-ietf-poly1305"
-AUUID="4abab2b5-16cc-4c4f-8bfb-887489f5dc82"
+#AUUID="4abab2b5-16cc-4c4f-8bfb-887489f5dc82"
 #PORT=80
 
 
@@ -16,7 +16,7 @@ mkdir -p /etc/caddy/ /etc/xray/ /usr/share/caddy && echo -e "User-agent: *\nDisa
 wget $CADDYIndexPage -O /usr/share/caddy/index.html 
 # wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
 # wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/etc/xray/config.json
-cat /conf/Caddyfile | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
+cat /conf/Caddyfile | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" >/etc/caddy/Caddyfile
 cat /conf/xray.json | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/etc/xray/config.json
 
 # start
